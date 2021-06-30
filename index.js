218a78bbd86850e37528e91245d191bac66ebe34
mdquayle <misternopants@gmail.com>
2021-06-30 18:48:39 -0400

    commit tree 0fddabff7b5b801f7e8c8513ba1089930ee670b1

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+const htmlBody = document.querySelector('body');
+
+function submitData(name,email){
+    const inputData = {
+        name: `${name}`,
+        email: `${email}`,
+      };
+      const configObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(inputData),
+    }
+    
+    return fetch("http://localhost:3000/users", configObject)
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(body) {
+        console.log(body)
+        htmlBody.textContent = body.id;
+      })
+      .catch(function(error) {
+        alert("ERROR ERROR EROR ERR E");
+        htmlBody.textContent = error.message; 
+      });
+}
